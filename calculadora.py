b238c9b56a4756d8f6d04a13e3d5609d9baa8956
NeyzerJss <neyzerjesus2005@gmail.com>
2025-03-06 09:36:46 -0500

    Unit Test

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+class Calculadora:
+    #metodo para sumar 2 modulos
+    def suma (self, a, b):
+        return a + b
+    #metodo para restar 2 modulos
+    def resta (self, a, b):
+        return a - b
+    #metodo para multiplicar 2 modulos
+    def multiplicacion (self, a, b):
+        return a * b
+    #metodo para dividir 2 modulos
+    def divisor (self, a, b):
+        return a / b
